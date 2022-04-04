a6c77718d16feb646ff40dc6600732e7c3884873
Rafael Rampim Soratto <rafael.soratto@appmoove.com.br>
2022-04-04 08:49:55 -0300

    add bot setup

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from github import Github, GithubIntegration
 
 app = Flask(__name__)
 
-app_id = '<your_app_number_here>'
+app_id = '186915'
 
 # Read the bot certificate
 with open(
-        os.path.normpath(os.path.expanduser('bot_key.pem')),
+        os.path.normpath(os.path.expanduser('bot-example.2022-04-04.private-key.pem')),
         'r'
 ) as cert_file:
     app_key = cert_file.read()
